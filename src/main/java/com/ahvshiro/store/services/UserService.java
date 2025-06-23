7e74e4cbab84d8847495f1d6328d4516e7ff44fc
ahvShiro <arthurshiro123@gmail.com>
2025-06-23 16:06:53 -0300

    style: code formatting

--- src/main/java/com/ahvshiro/store/services/UserService.java
+++ src/main/java/com/ahvshiro/store/services/UserService.java
@@ -47,13 +47,14 @@ public class UserService {
 
 	public User update(Long id, User obj) {
 		try {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
 	}
+
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
